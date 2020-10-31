14c577f89cdccc287cde52c57e439dd966328c26
M-A <marek@adameczek.pl>
2020-10-31 12:50:22 +0100

    Update picture of development board.

---
+++ GsmModem/Models/GsmModemData.cs
@@ -0,0 +1,16 @@
+﻿using CommonClasses.Interfaces;
+using System;
+
+namespace GsmModem.Models
+{
+    public class GsmModemData : IMessage
+    {
+        public int? Id { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string MessageBody { get; set; }
+        public bool? IsProcessed { get; set; }
+        public Guid ActorId { get; set; }
+        public double? Temperature { get; set; }
+        public double? Humidity { get; set; }
+    }
+}
